6586866e9bf0538c35d031acb4b46d27453b732b
Alejandro Pacheco <ale_pach03@hotmail.com>
2023-05-12 16:41:16 -0700

    Update Gameboard

---
+++ DrawBoard.py
@@ -0,0 +1,25 @@
+import pygame
+
+
+WIDTH = 900
+HEIGHT = 900
+ROWS = 3
+COLS = 3
+WHITE = (0, 0, 0)
+
+surface = pygame.display.set_mode((900,900))
+
+def drawSquare():
+    squareSize = 500
+    
+    color = (250,250,250)
+    pygame.draw.rect(surface, color, pygame.Rect(150,150,600,600))
+
+
+
+def drawGrid():
+    blockSize = 200
+    for x in range(150, 750, blockSize):
+        for y in range(150, 750, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(surface, WHITE, rect, 10)
--- Game.py
+++ Game.py
@@ -1,4 +1,23 @@
 import pygame
+from DrawBoard import drawSquare, drawGrid
 
+# Constants
 pygame.init()
+drawSquare()
+drawGrid()
+
+running = True
+while running:
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            running = False
+
+        pygame.display.flip()
+pygame.quit()
+
+
+
+
 
